a8ab2346120c1e882ef88219e854e6fc3df59670
sjoerdk <sjoerd.kerkstra@radboudumc.nl>
2026-01-12 16:18:20 +0100

    Tables are now Radboud format

--- examples/generate_dummy_protocol_pdf.py
+++ examples/generate_dummy_protocol_pdf.py
@@ -9,6 +9,7 @@ from midomtoolbox.render.markdown import render_protocol
 protocol = ProtocolFactory.build()
 protocol.sort_tags()  # Sort from specific to general
 
+
 md_protocol = render_protocol(protocol)
 
 html = markdown.markdown(md_protocol, extensions=["tables"])
@@ -43,13 +44,22 @@ table{
     border-collapse:collapse;
     border-spacing:0;
     border-collapse: collapse;
+    border-bottom: 1pt solid #00AFDC;
 
 }
+th{
+    background-color: #00AFDC;
+    color:white;
+}
+td {
+    color: black;
+}
 td,th{
+
     padding:0;
     font-size:0.6rem;
     line-height:0.8rem;
-    border: 1px solid;
+    border-left: 0.5pt solid white;
     padding: 0.5em;
 }
 
@@ -57,7 +67,12 @@ td,th{
     font_config=font_config,
 )
 
+html_out_debug = "/home/sjoerd/Downloads/output.html"
 output_file = "/tmp/output.pdf"
+
+with open(html_out_debug, "w") as f:
+    f.write(html)
+
 
 HTML(string=html).write_pdf(output_file, stylesheets=[css])
 
--- midomtoolbox/render/markdown.py
+++ midomtoolbox/render/markdown.py
@@ -1,8 +1,16 @@
 """Writing MIDOM protocol files as markup files"""
-from typing import List
+from typing import Dict, List
 
 from jinja2 import Environment, PackageLoader
-from midom.components import PrivateAllowGroup, Protocol, TagAction
+from midom.components import (
+    Filter,
+    PixelOperation,
+    PrivateAllowGroup,
+    Protocol,
+    TagAction,
+)
+from pydantic import BaseModel
+from pydicom.uid import UID
 
 jinja_env = Environment(loader=PackageLoader("midomtoolbox", "templates"))
 
@@ -40,14 +48,41 @@ def render_private_tags_table(private=List[PrivateAllowGroup]):
     return output
 
 
+class ProtocolContents(BaseModel):
+    """Contains all contents that should be sent to jinja.
+
+    Separated this from Protocol itself to be free to alter internals, rewrite text
+    etc., just for rendering purposes. I don't want to pass along a Protocol instance
+    that actually is not a Protocol any more.
+    """
+
+    tags: Dict[str, List[TagAction]]
+    filters: List[Filter]
+    pixel: List[PixelOperation]
+    private: List[PrivateAllowGroup]
+
+    @classmethod
+    def init_from_protocol(cls, protocol_in):
+        return cls(
+            tags=protocol_in.tags,
+            filters=protocol_in.filters,
+            pixel=protocol_in.pixel,
+            private=protocol_in.private,
+        )
+
+
 def render_protocol(protocol: Protocol):
     """Render tags list to a markup table"""
 
     # convert tags here as jinja cannot run the TagAction.identifier.key() method..
-
-    protocol.tags = {
-        sop_class: to_tags_table_contents(tag_actions)
-        for sop_class, tag_actions in protocol.tags.items()
+    pc = ProtocolContents.init_from_protocol(protocol)
+    pc.tags = {
+        f"{UID(sop_class).name} ({sop_class})": to_tags_table_contents(
+            tag_actions
+        )
+        for sop_class, tag_actions in pc.tags.items()
     }
-    output = jinja_env.get_template("protocol.md.j2").render(protocol=protocol)
+    output = jinja_env.get_template("protocol.md.j2").render(
+        protocol_contents=pc
+    )
     return output
